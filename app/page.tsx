6eb5b628185c2e9c60e30fbbb7432b32a3bbcaab
ilyasilmek <ilyas-ilmek@hotmail.com>
2026-09-01 14:35:51 +0300

    chore: initialize project foundation

---
+++ app/page.tsx
@@ -0,0 +1,3 @@
+export default function HomePage() {
+  return <main><h1>🔬 Mesaj Otopsisi</h1><p>“Tamam” gerçekten tamam mı?</p><button type="button">Ekran Görüntüsü Yükle</button><button type="button">Metin Gir</button></main>;
+}
